46c8a5f0f177e733a9209260ef6340f67ee8618d
manlikevee <odahviktor@gmail.com>
2024-05-03 22:48:35 +0100

    bbbbasdas

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 var swiper = new Swiper(".swiper-panorama", {
-    loop: true,
-    slidesPerView: 'auto',
-    centeredSlides: false,
-    loopAddBlankSlides: true,
-    speed: 600,
-    spaceBetween: 0,
-    mousewheel: false, 
-    effect: "coverflow",
-    coverflowEffect: {
-        stretch: -5,
-        rotate: 10,
-        depth: -65,
-        modifier: 2,
-        slideShadows: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 2000, // Time in milliseconds between slides
-        disableOnInteraction: false, // Autoplay will not be disabled after user interactions (swipes, clicks, etc.)
-    },
+  loop: true,
+  slidesPerView: 'auto',
+  centeredSlides: false,
+  loopAddBlankSlides: true,
+  speed: 600,
+  spaceBetween: 0,
+  mousewheel: false, 
+  effect: "coverflow",
+  coverflowEffect: {
+      stretch: -5,
+      rotate: 10,
+      depth: -65,
+      modifier: 2,
+      slideShadows: false,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+      delay: 2000, // Time in milliseconds between slides
+      disableOnInteraction: false, // Autoplay will not be disabled after user interactions (swipes, clicks, etc.)
+  },
+  breakpoints: {
+      // When window width is >= 576px
+      576: {
+          slidesPerView: 1,
+          spaceBetween: 10
+      },
+      // When window width is >= 768px
+      768: {
+          slidesPerView: 3,
+          spaceBetween: 10
+      },
+      // When window width is >= 992px
+      992: {
+          slidesPerView: 4,
+          spaceBetween: 10
+      }
+      // Add more breakpoints as needed
+  }
 });
-
 
 
 
